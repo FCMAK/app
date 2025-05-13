ad329e275464e7e16a5e91aeee2288ce8cb59248
Licethd <liceth.dfe@gmail.com>
2025-05-12 23:31:30 -0400

    f55

--- src/Services/Kolping/Components/servicio_informacion/Pages/Lista.js
+++ src/Services/Kolping/Components/servicio_informacion/Pages/Lista.js
@@ -36,14 +36,14 @@ class Lista extends Component {
                         </SView>
                     }
                 },
-                {
-                    key: "key-eliminar", label: "Eliminar", width: 70, center: true,
-                    component: (key) => {
-                        return <SView width={35} height={35} onPress={() => { SPopup.confirm({ title: "Eliminar", message: "¿Esta seguro de eliminar?", onPress: () => { Parent.Actions.eliminar(data_informacion[key], this.props) } }) }}>
-                            <SIcon name={'Delete'} />
-                        </SView>
-                    }
-                },
+                // {
+                //     key: "key-eliminar", label: "Eliminar", width: 70, center: true,
+                //     component: (key) => {
+                //         return <SView width={35} height={35} onPress={() => { SPopup.confirm({ title: "Eliminar", message: "¿Esta seguro de eliminar?", onPress: () => { Parent.Actions.eliminar(data_informacion[key], this.props) } }) }}>
+                //             <SIcon name={'Delete'} />
+                //         </SView>
+                //     }
+                // },
                 {
                     key: "key-editar", label: "Promos", width: 50, center: true,
                     component: (item) => {
